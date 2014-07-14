bd61eb2e25252a93e94daab540ef88f275b87354
Dominic Smith <dominic.smith@bristol.ac.uk>
2014-07-14 21:42:22 +0100

    updated

---
+++ setup.sh
@@ -0,0 +1,4 @@
+export EXTERNAL=/usr/local/Cellar/hepmc/2.06.09/
+export LD_LIBRARY_PATH=/usr/local/Cellar/gfortran/4.8.2/gfortran/lib:/usr/local/Cellar/hempc/2.06.09/lib:/usr/local/Cellar/hepmc/2.06.09//lib:$LD_LIBRARY_PATH
+
+
